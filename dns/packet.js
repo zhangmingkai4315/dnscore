1f8f97cd5447c864184ade2590fc9290f12aa62c
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2017-04-02 18:31:48 +0800

    add dns parse for a and ns

--- dns/packet.js
+++ dns/packet.js
@@ -90,7 +90,14 @@ dns.prototype.getBuffer = function(){
  */
 
 function dnspacket(buf){
+    /* buffer from socket */ 
     this.buffer = buf;
+    /* offset for read buf */ 
+    this.offset = 0;
+    /* cache for message compression */ 
+    this.cache ={
+
+    }
     this.header = {  
     }
     this.question = [];
@@ -114,13 +121,127 @@ dnspacket.prototype.parse_header=function(){
     this.answer.length =this.buffer.readUInt16BE(6);
     this.authority.length = this.buffer.readUInt16BE(8);
     this.additional.length =this.buffer.readUInt16BE(10);
+    this.offset = 12;
+}
+dnspacket.prototype.parse_question=function(){
+    var len = this.question.length;
+    if(len===0){
+        return
+    }
+    for(var i=0;i<len;i++){
+        var offset = this.offset;
+        var domainName=''
+        var readlen= this.buffer.readUIntBE(offset,1);
+        var recorder = '';
+        var keyList = [];
+        while(readlen){      
+            recorder = this.buffer.toString('ascii',offset+1,readlen+offset+1)+'.'
+            domainName+=recorder;
+            this.cache[offset]=recorder;
+            for(var i of keyList){
+                this.cache[i]=this.cache[i]+recorder;
+            }
+            keyList.push(offset);
+            offset = readlen+offset+1;
+            readlen = this.buffer.readUIntBE(offset,1);
+        }
+        
+        var type = this.buffer.readUInt16BE(offset+1);
+        var classin = this.buffer.readUInt16BE(offset+3);
+        this.question.shift()
+        this.question.push({domain:domainName,type:type,class:classin});
+        this.offset=offset+5
+    }
+}
+dnspacket.prototype.parseType=function(type,data){
+    switch(type){
+        case dns_const.QUERY.A:{
+            return this.parse_A(data);
+        }
+        case dns_const.QUERY.NS:{
+            return this.parse_NS(data);
+        }
+        default:
+            return '';
+        }
 }
 
+dnspacket.prototype._getDomainName=function(data){
+            var offset = 0;
+            var beginPoint = this.offset;
+            var domainName=''
+            var readlen= data.readUIntBE(offset,1);
+            var recorder = '';
+            var keyList = [];
+            var cache=[];
+            while(readlen){
+                if(readlen>64){
+                    var _p= data.readUInt16BE(offset);
+                    pointer = _p&0x03fff
+                    recorder=this.cache[pointer]
+                    offset+=2;
+                    cache[this.offset+offset]=recorder;
+                }else{
+                    recorder = data.toString('ascii',offset+1,readlen+offset+1)+'.';
+                    // cache[this.offset+offset]=recorder;
+                    keyList.push(this.offset+offset);
+                    offset=offset+readlen+1;
+                }
+                domainName+=recorder;
+                for(var i of keyList){
+                    cache[i]=(cache[i]||'')+recorder;
+                }
+                if(offset >= data.length){
+                    break;
+                }
+                readlen = data.readUIntBE(offset,1);
+            }
+            this.cache=Object.assign({},this.cache,cache);
+            this.offset = this.offset+offset;
+            return domainName
+}
+
+dnspacket.prototype.parse_A=function(data){
+    var ip = []
+    for(var i=0;i<4;i++){
+        ip.push(data.readUInt8(i))
+    }
+    this.offset+=4;
+    return ip.join('.');
+}
+
+dnspacket.prototype.parse_NS=function(data){
+    return this._getDomainName(data);
+}
+dnspacket.prototype.parse_zones=function(){
+    for(var zone of [this.answer,this.authority,this.additional]){
+        var len =zone.length;
+        if(len===0){
+            return
+        }
+        for(var i=0;i<len;i++){
+            var domainName=this._getDomainName(this.buffer.slice(this.offset))
+            var offset = this.offset;
+            var type = this.buffer.readUInt16BE(offset);
+            var classin = this.buffer.readUInt16BE(offset+2);
+            var ttl =  this.buffer.readUInt32BE(offset+4);
+            var length =  this.buffer.readUInt16BE(offset+8);
+            var data =  this.buffer.slice(offset+10,offset+10+length);
+            
+            this.offset=offset+10
+            zone.shift()
+            zone.push({domain:domainName,type:type,class:classin,ttl:ttl,length:length,data:this.parseType(type,data)});
+            
+        }
+    }
+}
 function parse(buf){
     var dns = new dnspacket(buf);
     dns.parse_header();
-
-    console.log(dns);
+    dns.parse_question();
+    dns.parse_zones();
+    delete dns.cache
+    return dns
 }
 
 
@@ -130,15 +251,14 @@ function parse(buf){
 /* example code*/
 var d = new dns({
     question:{
-        name:'jsmean.com'
+        name:'baidu.com'
     }
 });
 const dgram = require('dgram');
 const message = d.getBuffer();
 const client = dgram.createSocket('udp4');
 client.on('message',(msg)=>{
-    console.log(msg);
-    parse(msg);
+    console.log(parse(msg));
     client.close();
 })
 
