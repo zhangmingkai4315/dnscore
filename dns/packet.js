a79954af43dc91a6edecc1784dbc6d758db9d2fe
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2017-04-06 13:50:42 +0800

    add eslint support

--- dns/packet.js
+++ dns/packet.js
@@ -1,4 +1,5 @@
 const dns_const = require('./dns_const');
+const Buffer = global.Buffer;
 
 const defaultOption={
     rd:dns_const.RD_DESIRED,
@@ -6,7 +7,7 @@ const defaultOption={
         type:dns_const.QUERY.A,
         name:dns_const.ROOT
     }
-}
+};
 
 /**
  * @constructor query dns packet
@@ -16,10 +17,10 @@ function dns(option){
     if(!option){
         option=defaultOption;
     }else{
-        option=Object.assign({},defaultOption,option)
+        option=Object.assign({},defaultOption,option);
     }
-    this.offset = 0
-    this.buffer = Buffer.allocUnsafe(512);
+    this.offset = 0;
+    this.buffer = Buffer.allocUnsafe(512); 
     this.offset = dns_header(this.buffer,this.offset,option);
     this.offset= dns_question(this.buffer,this.offset,option);
     return this;
@@ -33,8 +34,8 @@ function dns(option){
  * @returns {Buffer} 
  */
 function dns_header(buf,offset,option){
-    buf.writeUInt16BE(Math.floor(Math.random() * Math.pow(2,16)),offset)
-    var flag = option.rd === dns_const.RD_DESIRED? 1<<8 : 0 
+    buf.writeUInt16BE(Math.floor(Math.random() * Math.pow(2,16)),offset);
+    var flag = option.rd === dns_const.RD_DESIRED? 1<<8 : 0;
     buf.writeUInt16BE(flag,offset+2);
     buf.writeUInt16BE(1,offset+4);
     buf.writeUInt16BE(0,offset+6);
@@ -55,18 +56,18 @@ function dns_question(buf,offset,option){
     var name = option.question.name||dns_const.ROOT;
     var classin =option.question.class||dns_const.CLASS_IN;
     for(var subdomain of name.split('.')){
-      if(subdomain!==''){
-        var domainlength=subdomain.length;
-        buf.writeUInt16BE(domainlength<<8,offset);
-        offset=offset+1;
-        buf.write(subdomain,offset,domainlength,'ascii');
-        offset+=subdomain.length;
-      }
+        if(subdomain!==''){
+            var domainlength=subdomain.length;
+            buf.writeUInt16BE(domainlength<<8,offset);
+            offset=offset+1;
+            buf.write(subdomain,offset,domainlength,'ascii');
+            offset+=subdomain.length;
+        }
     }
     buf.writeUInt16BE(0x00&0xFFFF,offset);
-    offset=offset+1
+    offset=offset+1;
     buf.writeUInt16BE(type&0xFFFF,offset);
-    offset=offset+2
+    offset=offset+2;
     buf.writeUInt16BE(classin&0xFFFF,offset);
     return offset+2;
 }
@@ -78,7 +79,7 @@ function dns_question(buf,offset,option){
  */
 dns.prototype.getBuffer = function(){
     return this.buffer.slice(0,this.offset);
-}
+};
 
 /* Unpack the dns buffer to a object */ 
 
@@ -96,14 +97,11 @@ function dnspacket(buf){
     /* offset for read buf */ 
     this.offset = 0;
     /* cache for message compression */ 
-    this.cache ={
-
-    }
-    this.header = {  
-    }
-    this.question = [];
-    this.answer = [];
-    this.authority = [];
+    this.cache={},
+    this.header={},
+    this.question=[],
+    this.answer = [],
+    this.authority = [],
     this.additional = [];
     return this;
 }
@@ -117,10 +115,10 @@ function parse(buf){
     dns.parse_header();
     dns.parse_question();
     dns.parse_zones();
-    delete dns.cache
-    delete dns.buffer
-    delete dns.offset
-    return dns
+    delete dns.cache;
+    delete dns.buffer;
+    delete dns.offset;
+    return dns;
 }
 /**
  * @description parse the raw buffer and retrive the dns header information.
@@ -142,7 +140,7 @@ dnspacket.prototype.parse_header=function(){
     this.authority.length = this.buffer.readUInt16BE(8);
     this.additional.length =this.buffer.readUInt16BE(10);
     this.offset = 12;
-}
+};
 
 /**
  * @description parse the raw buffer and retrive the dns questions information.
@@ -152,20 +150,20 @@ dnspacket.prototype.parse_header=function(){
 dnspacket.prototype.parse_question=function(){
     var len = this.question.length;
     if(len===0){
-        return
+        return;
     }
     for(var i=0;i<len;i++){
         var offset = this.offset;
-        var domainName=''
+        var domainName='';
         var readlen= this.buffer.readUIntBE(offset,1);
         var recorder = '';
         var keyList = [];
         while(readlen){      
-            recorder = this.buffer.toString('ascii',offset+1,readlen+offset+1)+'.'
+            recorder = this.buffer.toString('ascii',offset+1,readlen+offset+1)+'.';
             domainName+=recorder;
             this.cache[offset]=recorder;
-            for(var i of keyList){
-                this.cache[i]=this.cache[i]+recorder;
+            for(var key of keyList){
+                this.cache[key]=this.cache[key]+recorder;
             }
             keyList.push(offset);
             offset = readlen+offset+1;
@@ -174,11 +172,11 @@ dnspacket.prototype.parse_question=function(){
         
         var type = this.buffer.readUInt16BE(offset+1);
         var classin = this.buffer.readUInt16BE(offset+3);
-        this.question.shift()
+        this.question.shift();
         this.question.push({domain:domainName,type:type,class:classin});
-        this.offset=offset+5
+        this.offset=offset+5;
     }
-}
+};
 
 /**
  * @description parse the raw buffer and retrive the dns (answer,authority,additional) information.
@@ -189,10 +187,10 @@ dnspacket.prototype.parse_zones=function(){
     for(var zone of [this.answer,this.authority,this.additional]){
         var len =zone.length;
         if(len===0){
-            continue
+            continue;
         }
         for(var i=0;i<len;i++){
-            var domainName=this._getDomainName(this.buffer.slice(this.offset),1)
+            var domainName=this._getDomainName(this.buffer.slice(this.offset),1);
             var offset = this.offset;
             var type = this.buffer.readUInt16BE(offset);
             var classin = this.buffer.readUInt16BE(offset+2);
@@ -200,12 +198,12 @@ dnspacket.prototype.parse_zones=function(){
             var length =  this.buffer.readUInt16BE(offset+8);
             var data =  this.buffer.slice(offset+10,offset+10+length);
             
-            this.offset=offset+10
-            zone.shift()
+            this.offset=offset+10;
+            zone.shift();
             zone.push({domain:domainName,type:type,class:classin,ttl:ttl,length:length,data:this.parseType(type,data)});
         }
     }
-}
+};
 
 /**
  * @description parse the raw buffer and domain name string(compressed pointer will be replaced with original string).
@@ -214,46 +212,47 @@ dnspacket.prototype.parse_zones=function(){
  * @returns {string} domainName
  */
 dnspacket.prototype._getDomainName=function(data,max_pointers){
-            var offset = 0;
-            var domainName=''
-            var readlen= data.readUIntBE(offset++,1);
-            var recorder = '';
-            var keyList = [];
-            var cache=[];
-            var pointerCounter = 0;
-            while(readlen){
-                if(readlen>64){
-                    var _p= data.readUInt16BE(offset-1);
-                    pointer = _p&0x03fff
-                    recorder=this.cache[pointer]
-                    offset++;
-                    cache[this.offset+offset]=recorder;
-                    pointerCounter++;
+    var offset = 0;
+    var domainName='';
+    var readlen= data.readUIntBE(offset++,1);
+    var recorder = '';
+    var keyList = [];
+    var cache=[];
+    var pointer =0;
+    var pointerCounter = 0;
+    while(readlen){
+        if(readlen>64){
+            var _p= data.readUInt16BE(offset-1);
+            pointer=_p&0x03fff;
+            recorder=this.cache[pointer];
+            offset++;
+            cache[this.offset+offset]=recorder;
+            pointerCounter++;
 
-                }else{
-                    recorder = data.toString('ascii',offset,readlen+offset)+'.';
-                    // cache[this.offset+offset]=recorder;
-                    keyList.push(this.offset+offset-1);
-                    offset=offset+readlen;
-                }
-                domainName+=recorder;
-                for(var i of keyList){
-                    cache[i]=(cache[i]||'')+recorder;
-                }
-                if(offset >= data.length){
-                    break;
-                }
-                if(max_pointers&&pointerCounter===max_pointers){
-                    break;
-                }
-                readlen = data.readUIntBE(offset++,1);
-            }
+        }else{
+            recorder = data.toString('ascii',offset,readlen+offset)+'.';
+            // cache[this.offset+offset]=recorder;
+            keyList.push(this.offset+offset-1);
+            offset=offset+readlen;
+        }
+        domainName+=recorder;
+        for(var i of keyList){
+            cache[i]=(cache[i]||'')+recorder;
+        }
+        if(offset >= data.length){
+            break;
+        }
+        if(max_pointers&&pointerCounter===max_pointers){
+            break;
+        }
+        readlen = data.readUIntBE(offset++,1);
+    }
 
-            this.cache=Object.assign({},this.cache,cache);
-            
-            this.offset = this.offset+offset;
-            return domainName
-}
+    this.cache=Object.assign({},this.cache,cache);
+    
+    this.offset = this.offset+offset;
+    return domainName;
+};
 
 /**
  * @description parse the raw buffer based its type.
@@ -265,35 +264,35 @@ dnspacket.prototype._getDomainName=function(data,max_pointers){
  */
 dnspacket.prototype.parseType=function(type,data){
     switch(type){
-        case dns_const.QUERY.A:{
-            return this.parse_A(data);
-        }
-        case dns_const.QUERY.NS:{
-            return this.parse_NS(data);
-        }
-        case dns_const.QUERY.TXT:{
-            return this.parse_TXT(data);
-        }
-        case dns_const.QUERY.MX:{
-            return this.parse_MX(data);
-        }
-        case dns_const.QUERY.AAAA:{
-            return this.parse_AAAA(data);
-        }
-        case dns_const.QUERY.SOA:{
-            return this.parse_SOA(data);
-        }
-        case dns_const.QUERY.CAA:{
-            return this.parse_CAA(data);
-        }
-        case dns_const.QUERY.CNAME:{
-            return this.parse_CNAME(data);
-        }
-        default:
-            this.offset+=data.length;
-            return data.toString('ascii');
-        }
-}
+    case dns_const.QUERY.A:{
+        return this.parse_A(data);
+    }
+    case dns_const.QUERY.NS:{
+        return this.parse_NS(data);
+    }
+    case dns_const.QUERY.TXT:{
+        return this.parse_TXT(data);
+    }
+    case dns_const.QUERY.MX:{
+        return this.parse_MX(data);
+    }
+    case dns_const.QUERY.AAAA:{
+        return this.parse_AAAA(data);
+    }
+    case dns_const.QUERY.SOA:{
+        return this.parse_SOA(data);
+    }
+    case dns_const.QUERY.CAA:{
+        return this.parse_CAA(data);
+    }
+    case dns_const.QUERY.CNAME:{
+        return this.parse_CNAME(data);
+    }
+    default:
+        this.offset+=data.length;
+        return data.toString('ascii');
+    }
+};
 
 /**
  * @description parse the raw buffer return A record.
@@ -301,26 +300,26 @@ dnspacket.prototype.parseType=function(type,data){
  * @returns {string} a IPv4 Address record
  */
 dnspacket.prototype.parse_A=function(data){
-    var ip = []
+    var ip = [];
     for(var i=0;i<4;i++){
-        ip.push(data.readUInt8(i))
+        ip.push(data.readUInt8(i));
     }
     this.offset+=4;
     return ip.join('.');
-}
+};
 /**
  * @description parse the raw buffer return AAAA(IPv6) record.
  * @param {Buffer} data
  * @returns {string} a AAAA record
  */
 dnspacket.prototype.parse_AAAA = function(data){
-    var ipv6 = []
+    var ipv6 = [];
     for(var i=0;i<8;i++){
-        ipv6.push(data.readUInt16BE(i*2).toString(16))
+        ipv6.push(data.readUInt16BE(i*2).toString(16));
     }
     this.offset+=16;
     return ipv6.join(':');
-}
+};
 
 /**
  * @description parse the raw buffer return SOA record.
@@ -341,9 +340,9 @@ dnspacket.prototype.parse_SOA = function(data){
         retry:left_data.readInt32BE(8),
         expire:left_data.readInt32BE(12),
         minTTL:left_data.readInt32BE(16)
-    }
+    };
     return obj;
-}
+};
 /**
  * @description parse the raw buffer return NameServer record.
  * @param {Buffer} data
@@ -351,7 +350,7 @@ dnspacket.prototype.parse_SOA = function(data){
  */
 dnspacket.prototype.parse_NS=function(data){
     return this._getDomainName(data);
-}
+};
 /**
  * @description parse the raw buffer return txt record.
  * @param {Buffer} data
@@ -361,7 +360,7 @@ dnspacket.prototype.parse_TXT = function(data){
     this.offset+=data.length;
     var txtLength = data.readUInt8(0);
     return data.toString('utf8',1,txtLength+1);
-}
+};
 
 /**
  * @description parse the raw buffer return MX record.
@@ -373,8 +372,8 @@ dnspacket.prototype.parse_MX = function(data){
     return  {
         preference:data.readUInt16BE(0),
         exchange:this._getDomainName(data.slice(2))
-    }
-}
+    };
+};
 
 
 /**
@@ -383,8 +382,8 @@ dnspacket.prototype.parse_MX = function(data){
  * @returns {string} a MX record
  */
 dnspacket.prototype.parse_CNAME = function(data){
-    return  this._getDomainName(data,1,data.length)
-}
+    return  this._getDomainName(data,1,data.length);
+};
 
 /**
  * @description parse the raw buffer return MX record.
@@ -396,8 +395,8 @@ dnspacket.prototype.parse_PTR = function(data){
     return  {
         preference:data.readUInt16BE(0),
         exchange:this._getDomainName(data.slice(2))
-    }
-}
+    };
+};
 /**
  * @description parse the raw buffer return CAA record.
  * @param {Buffer} data
@@ -405,7 +404,6 @@ dnspacket.prototype.parse_PTR = function(data){
  * @see {@link https://tools.ietf.org/html/rfc6844|RFC6844}
  */
 dnspacket.prototype.parse_CAA = function(data){
-    console.log(data);
     var flags = data.readIntBE(0,1);
     var caa_tags_length = data.readIntBE(1,1);
     var tag = data.slice(2,caa_tags_length+2).toString('utf8');
@@ -415,10 +413,11 @@ dnspacket.prototype.parse_CAA = function(data){
         flags,
         tag,
         value,
-    }
-}
+    };
+};
 
 if((module===require.main)){
+    /* eslint-disable no-console */
     const dgram = require('dgram');
     var client = dgram.createSocket('udp4');
     var d = new dns({
@@ -428,21 +427,24 @@ if((module===require.main)){
         }
     });
     client.on('message',(msg)=>{
-        var p = parse(msg); 
-        console.log(p)
+        
+        console.log(parse(msg));  
         client.close();
     });
     client.send(d.getBuffer(), 53, '1.2.4.8', (err) => {
         if(err){
-            done(err);
+            console.log(err);
         }
     });
+    /* eslint-enable no-console */
+}else{
+    module.exports = {
+        dnspacket,
+        parse,
+        dns
+    };
 }
 
-module.exports = {
-    dnspacket,
-    parse,
-    dns,
-}
+
 
 
